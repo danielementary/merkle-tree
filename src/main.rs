dc32c76b3d0bdab6ded3d41ab6087f17d6c67c0f
Daniel Filipe Nunes Silva <danielementary@proton.me>
2022-12-12 00:22:03 +0100

    shift nodes by 1 and add error handling

--- src/main.rs
+++ src/main.rs
@@ -23,42 +23,62 @@ struct MerkleTree {
     hash_function: HashFunction,
     height: usize,
     length: usize,
+    first_leaf_node_index: usize,
     nodes: Vec<Option<Node>>,
 }
 
 impl MerkleTree {
     fn from_height(hash_function: HashFunction, height: usize) -> Self {
+        if height <= 0 || height > 10 {
+            panic!(
+                "The height of the merkle tree cannot be less or equal to 0 or greater than 10."
+            );
+        }
+
         MerkleTree {
             hash_function,
             height,
             length: 0,
-            nodes: vec![None; Self::sum_of_powers_of_two(height)],
+            first_leaf_node_index: Self::sum_of_powers_of_two(height - 1) + 1,
+            nodes: vec![None; Self::sum_of_powers_of_two(height) + 1],
         }
     }
 
     fn insert(mut self, value: String) {
-        let index = Self::sum_of_powers_of_two(self.height - 1) + self.length;
+        let next_leaf_node_index = self.first_leaf_node_index + self.length;
+
+        if next_leaf_node_index >= self.nodes.len() {
+            panic!("The merkle tree is already full.")
+        }
+
         let hash = (self.hash_function)(value);
 
-        self.nodes[index] = Some(Node { hash });
+        self.nodes[next_leaf_node_index] = Some(Node { hash });
         self.length += 1;
 
-        let mut i = index / 2;
-        for _ in 0..=self.height {
+        let mut i = next_leaf_node_index / 2;
+        for _ in 0..self.height {
             self.nodes[i] = None;
             i /= 2;
         }
     }
 
     fn get_node_hash(&self, index: usize) -> Hash {
+        if index >= self.nodes.len() {
+            panic!("This node is out of bounds.");
+        }
+
         match &self.nodes[index] {
             Some(node) => node.hash.clone(),
-            None => (self.hash_function)("empty node".to_string()),
+            None if index >= self.first_leaf_node_index => {
+                (self.hash_function)("empty node".to_string())
+            }
+            _ => panic!("Internal nodes cannot be None."),
         }
     }
 
     fn update_internal_nodes(&mut self) {
-        for i in (0..Self::sum_of_powers_of_two(self.height - 1)).rev() {
+        for i in (1..self.first_leaf_node_index).rev() {
             if self.nodes[i].is_none() {
                 let left_child_index = 2 * i;
 
@@ -74,14 +94,17 @@ impl MerkleTree {
     }
 
     fn get_root(self) -> Hash {
-        match &self.nodes[0] {
-            Some(root) => root.hash.clone(),
-            None => "error".to_string(),
-        }
+        self.get_node_hash(1)
+    }
+
+    fn get_value(self, value_index: usize) -> Hash {
+        let index = self.first_leaf_node_index + value_index;
+
+        self.get_node_hash(index)
     }
 
     fn get_opening(self, value_index: usize) -> Opening {
-        let node_index = Self::sum_of_powers_of_two(self.height - 1) + value_index;
+        let node_index = self.first_leaf_node_index + value_index;
 
         let partner_index;
         if (node_index % 2 == 0) {
