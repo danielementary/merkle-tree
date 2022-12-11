ea2bc1ec48f5caa8e85010b9527cbe53d8e92ec2
Daniel Filipe Nunes Silva <danielementary@proton.me>
2022-12-11 23:07:12 +0100

    add MT opening struct and get method

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ struct Node {
     hash: String,
 }
 
+struct Opening {
+    partner_hash: String,
+    root_child_hash: String,
+}
+
 struct MerkleTree {
     hash_function: HashFunction,
     height: usize,
@@ -35,11 +40,10 @@ impl MerkleTree {
         let hash = (self.hash_function)(value);
 
         self.nodes[index] = Some(Node { hash });
-
         self.length += 1;
 
         let mut i = index / 2;
-        while i > 0 {
+        for _ in 0..=self.height {
             self.nodes[i] = None;
             i /= 2;
         }
@@ -72,6 +76,27 @@ impl MerkleTree {
         match &self.nodes[0] {
             Some(root) => root.hash.clone(),
             None => "error".to_string(),
+        }
+    }
+
+    fn get_opening(self, value_index: usize) -> Opening {
+        let node_index = Self::sum_of_powers_of_two(self.height - 1) + value_index;
+
+        let partner_index;
+        if (node_index % 2 == 0) {
+            partner_index = node_index + 1;
+        } else {
+            partner_index = node_index - 1;
+        }
+
+        let mut root_child_index = node_index;
+        for _ in 0..(self.height - 1) {
+            root_child_index /= 2;
+        }
+
+        Opening {
+            partner_hash: self.get_node_hash(partner_index),
+            root_child_hash: self.get_node_hash(root_child_index),
         }
     }
 
