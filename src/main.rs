a77487761c3aa6bec14f2b5cce3e005edbae51e3
Daniel Filipe Nunes Silva <danielementary@proton.me>
2022-12-11 21:33:40 +0100

    add hash function fn and from_height method for MT

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
+type HashFunction = fn(String) -> String;
+
 fn main() {
     println!("Hello, merkle tree!");
 }
 
+fn dummy_hash(input: String) -> String {
+    "Hash of ({input})".to_string()
+}
+
+#[derive(Clone)]
 struct Node {
     hash: String,
 }
+
 struct MerkleTree {
+    hash_function: HashFunction,
     height: usize,
     length: usize,
-    nodes: Vec<Node>,
+    nodes: Vec<Option<Node>>,
+}
+
+impl MerkleTree {
+    fn from_height(hash_function: HashFunction, height: usize) -> Self {
+        MerkleTree {
+            hash_function,
+            height,
+            length: 0,
+            nodes: vec![None; Self::sum_of_powers_of_two(height)],
+        }
+    }
+
+    fn sum_of_powers_of_two(n: usize) -> usize {
+        2 ^ (n + 1) - 1
+    }
 }
