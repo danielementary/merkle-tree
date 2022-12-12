e42c856bd65f6b6ddc535a930fdb413a86ad63ab
danielementary <danielementary@proton.me>
2022-12-12 12:40:14 +0100

    add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,35 @@
 pub type Hash = String;
 pub type HashFunction = fn(String) -> Hash;
 
+/// a single node of the merkle tree
 #[derive(Clone)]
 struct Node {
+    /// a node only contains the hash corresponding to its position in the merkle tree
     hash: Hash,
 }
 
 pub struct MerkleTree {
     hash_function: HashFunction,
     height: usize,
+    /// the `length` represents the number of elements inserted in the merkle tree
     length: usize,
+    /// `first_leaf_node_index` corresponds to
+    /// the index of the first inserted element in the merkle tree
     first_leaf_node_index: usize,
+    /// the merkle tree is implemented as a linear array of `Option<Node>`,
+    /// where the nodes are sorted in a breadth first fashion
+    /// `nodes[0]` is always `None`
+    /// `nodes[1]` is always `Some(node)`, where `node` is the root of the merkle tree
+    /// the follwing nodes up to `first_leaf_node_index` correspond to
+    /// the internal nodes of the merkle tree followed by
+    /// the external nodes, which correspond to the hash of the inserted values
     nodes: Vec<Option<Node>>,
 }
 
 impl MerkleTree {
+    /// create an empty merkle tree with the provided `hash_function` and `height`
+    ///
+    /// panic if the height is less or equal to 0 or greater than 10
     pub fn from_height(hash_function: HashFunction, height: usize) -> Self {
         if height <= 0 || height > 10 {
             panic!(
@@ -26,11 +41,17 @@ impl MerkleTree {
             hash_function,
             height,
             length: 0,
+            // internal nodes are stored from 1 to 1 + 2 + ... + 2^(height - 1) + 1
             first_leaf_node_index: Self::sum_of_powers_of_two(height - 1) + 1,
+            // we need 1 empty slot for the first `None` + 1 + 2 + ... 2^height to store the merkle tree
             nodes: vec![None; Self::sum_of_powers_of_two(height) + 1],
         }
     }
 
+    /// insert a new value into the merkle tree and
+    /// set to `None` all the corresponding parents up to the root
+    ///
+    /// panic if the merkle tree is already full
     pub fn insert(&mut self, value: String) {
         let next_leaf_node_index = self.first_leaf_node_index + self.length;
 
@@ -43,21 +64,28 @@ impl MerkleTree {
         self.nodes[next_leaf_node_index] = Some(Node { hash });
         self.length += 1;
 
-        let mut i = next_leaf_node_index / 2;
+        // find parents up to the root
+        // by iteratively performing an integer division of the index by 2 and
+        // set them to `None`
+        let mut i = next_leaf_node_index;
         for _ in 0..self.height {
-            self.nodes[i] = None;
             i /= 2;
+            self.nodes[i] = None;
         }
     }
 
+    /// update the state of the internal nodes
+    /// by computing iteratively from the last internal node to the root
     pub fn update_internal_nodes(&mut self) {
         for i in (1..self.first_leaf_node_index).rev() {
+            // compute only nodes set to `None`
             if self.nodes[i].is_none() {
                 let left_child_index = 2 * i;
 
                 let left_child_hash = self.get_node_hash(left_child_index);
                 let right_child_hash = self.get_node_hash(left_child_index + 1);
 
+                // the hash of a node is the hash of the concatenation of its children's hashes
                 let hash =
                     (self.hash_function)(format!("{} | {}", left_child_hash, right_child_hash));
 
@@ -66,16 +94,26 @@ impl MerkleTree {
         }
     }
 
+    /// get the root hash
+    ///
+    /// panic if the root is `None`
     pub fn get_root(&self) -> Hash {
         self.get_node_hash(1)
     }
 
+    /// get the node hash corresponding to the `value_index`'th value inserted into the merkle tree
+    ///
+    /// panic if the node is out of bounds
     pub fn get_value(&self, value_index: usize) -> Hash {
         let index = self.first_leaf_node_index + value_index;
 
         self.get_node_hash(index)
     }
 
+    /// get a node hash corresponding to its position in the merkle tree
+    ///
+    /// panic if the node is out of bounds or
+    /// if the node is internal and `None`, i.e. not computed with `update_internal_nodes`
     fn get_node_hash(&self, index: usize) -> Hash {
         if index >= self.nodes.len() {
             panic!("This node is out of bounds.");
@@ -83,6 +121,7 @@ impl MerkleTree {
 
         match &self.nodes[index] {
             Some(node) => node.hash.clone(),
+            // if the node is external and was not inserted yet, it is replaced by "empty node" hash
             None if index >= self.first_leaf_node_index => {
                 (self.hash_function)("empty node".to_string())
             }
@@ -90,6 +129,7 @@ impl MerkleTree {
         }
     }
 
+    /// formula to compute 1 + 2 + ... + 2^n
     fn sum_of_powers_of_two(n: usize) -> usize {
         2usize.pow((n + 1) as u32) - 1
     }
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,24 @@ use merkle_tree::{Hash, HashFunction, MerkleTree};
 fn main() {
     println!("Hello, merkle tree!");
 
+    // define a dummy hash function that hashes "x" into "Hash of (x)"
     let dummy_hash: HashFunction = |input: String| -> Hash { format!("Hash of ({})", input) };
+
+    // create a merkle tree with the given dummy function and height of 2
     let mut mt = MerkleTree::from_height(dummy_hash, 2);
 
+    // insert three elements into the merkle tree
     mt.insert("Hello".to_string());
     mt.insert("Merkle".to_string());
     mt.insert("Tree".to_string());
 
+    // update the state of the internal nodes
     mt.update_internal_nodes();
 
+    // get and print the root's hash of the merkle tree
     println!("root: {}", mt.get_root());
 
+    // get and print the hash corresponding to value "Tree"
     let value_index = 2;
-    let value = mt.get_value(value_index);
-
-    println!("value: {}", value);
+    println!("value {}: {}", value_index, mt.get_value(value_index));
 }
